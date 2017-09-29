7e2581c8e0f3ec1e3f81ee1af4393e3ac3676381
github username <github email address>
2017-09-29 13:54:16 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+def collect_multiples(range)
+  results = []
+  (1...range ).each {|num| results << num if num % 3 == 0 || num % 5 == 0 }
+  results
+end
+
+def sum_multiples(range)
+  results = collect_multiples(range)
+  sum = 0
+  results.each {|num| sum += num}
+  sum
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,22 @@
 # Enter your object-oriented solution here!
+require "pry"
+class Multiples
+  attr_reader :range, :results
+  def initialize(range)
+    @range = range
+  end
+
+  def collect_multiples
+    @results = []
+    (1...@range ).each {|num| results << num if num % 3 == 0 || num % 5 == 0 }
+    @results
+  end
+
+  def sum_multiples
+    binding.pry
+    @results = collect_multiples
+    sum = 0
+    @results.each {|num| sum += num}
+    sum
+  end
+end
